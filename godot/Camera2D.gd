5047366cdf20911a46c75fa1e26de7e5f45a1fff
Yaniv Or <yanivo@welldone-software.com>
2019-09-24 11:27:46 +0300

    fixes to vehicle and chasis

---
+++ godot/Camera2D.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+var player_target
+var player_died = false
+
+func _ready():
+	player_target = get_parent().get_node("Vehicle")
+	pass
+
+func _process(delta):
+	if player_died:
+		pass
+		
+	position = player_target.get_position()
--- godot/Vehicle.gd
+++ godot/Vehicle.gd
@@ -1,14 +1,11 @@
 extends KinematicBody2D
 
-#onready var player = get_node("Animation")
-
 const MOVE_SPEED = 400
 const GRAVITY = 20
 const JUMP_FORCE = -850
 const FLOOR = Vector2(0, -1)
 
 var movement = Vector2(0, 0)
-var anim
 var on_floor = false
 
 func _physics_process(delta):
@@ -17,20 +14,17 @@ func _physics_process(delta):
 func playerMovement():
 	if Input.is_action_pressed("ui_right"):
 		movement.x = MOVE_SPEED
-#		player.set_flip_h(false)
-		anim = "Walk"
+		$AnimationPlayer.play("moving-1")
 	elif Input.is_action_pressed("ui_left"):
 		movement.x = -MOVE_SPEED
-#		player.set_flip_h(true)
-		anim = "Walk"
+		$AnimationPlayer.play("moving-1")
 	else:
 		movement.x = 0
-		anim = "Idle"
+		$AnimationPlayer.play("idle")
 	
 	if Input.is_action_pressed("ui_up") and on_floor:
 		movement.y = JUMP_FORCE
 		
-#	player.play(anim)
 	movement.y += GRAVITY
 	
 	if is_on_floor():
