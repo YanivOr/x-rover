0cd3f2695353ee04a85c73bc4c2d52a1872a9e0b
Yaniv Or <yanivo@welldone-software.com>
2019-09-24 15:26:32 +0300

    splitted vehicle anim to right and left

--- godot/Vehicle.gd
+++ godot/Vehicle.gd
@@ -2,22 +2,23 @@ extends KinematicBody2D
 
 const MOVE_SPEED = 400
 const GRAVITY = 20
-const JUMP_FORCE = -850
+const JUMP_FORCE = -500
 const FLOOR = Vector2(0, -1)
 
 var movement = Vector2(0, 0)
 var on_floor = false
 
+# warning-ignore:unused_argument
 func _physics_process(delta):
 	playerMovement()
 	
 func playerMovement():
 	if Input.is_action_pressed("ui_right"):
 		movement.x = MOVE_SPEED
-		$AnimationPlayer.play("moving-1")
+		$AnimationPlayer.play("move-right")
 	elif Input.is_action_pressed("ui_left"):
 		movement.x = -MOVE_SPEED
-		$AnimationPlayer.play("moving-1")
+		$AnimationPlayer.play("move-left")
 	else:
 		movement.x = 0
 		$AnimationPlayer.play("idle")
