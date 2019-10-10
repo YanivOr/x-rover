90ff5c479bc77d45a38df173dadbbacfe7f3e26b
Yaniv Or <yanivo@welldone-software.com>
2019-10-10 08:59:27 +0300

    added door

--- godot/HUD.gd
+++ godot/HUD.gd
@@ -1,9 +1,13 @@
 extends CanvasLayer
 
-func _ready():
-	
-	pass
-
 func _process(delta):
 	$"score-value".text = String(global.score)
 	$"lives-value".text = String(global.lives)
+
+func show_key():
+	var key = preload("res://Key.tscn").instance()
+	key.scale.x = 0.3
+	key.scale.y = 0.3
+	key.position.x = get_viewport().size.x - 50
+	key.position.y = 30
+	add_child(key)
