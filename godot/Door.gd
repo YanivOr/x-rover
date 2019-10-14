f8854ae43ba1fdd8f4d893911e587006f7a25d32
Yaniv Or <yanivo@welldone-software.com>
2019-10-14 17:16:11 +0300

    deleted old project

--- godot/Door.gd
+++
@@ -1,7 +0,0 @@
-extends Area2D
-
-
-
-func _on_Door_body_entered(body):
-	if body.get_name() == 'Vehicle':
-		print("Level completed")
--- godot/Potion.gd
+++
@@ -1,6 +0,0 @@
-extends Area2D
-
-func _on_Potion_body_entered(body):
-	if body.get_name() == 'Vehicle':
-		global.incrementLives()
-		queue_free()
